576aad27cf391caa2c70b65c3d6cc3d9e0791143
Mary Sam <marysam01@hotmail.com>
2015-08-30 13:25:53 -0700

    First commit

---
+++ BedrockBank/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    /// <summary>
+    /// BankAccountTraining
+    /// </summary>
+    class BankAccount
+    {
+        #region properties
+        /// <summary>
+        /// Account Balance
+        /// </summary>
+        public decimal Balance { get; set; }
+        /// <summary>
+        /// Name of Account
+        /// </summary>
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int AccountNumber { get; set; }
+        #endregion
+    }
+}
