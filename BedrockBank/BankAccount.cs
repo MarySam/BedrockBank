19d3209ae5e466b679a2503686f3beb0e9471dd5
Mary Sam <marysam01@hotmail.com>
2015-09-06 13:06:37 -0700

    Properties (Get and Set) Static/Instance (new) Constructor

--- BedrockBank/BankAccount.cs
+++ BedrockBank/BankAccount.cs
@@ -12,14 +12,18 @@ namespace BedrockBank
     /// </summary>
     class BankAccount
     {
+        #region Variables
+        static int lastAccountNumber = 0;
+        #endregion
+
         #region properties
         /// <summary>
         /// Account Balance
         /// </summary>
-        public decimal Balance { get; set; }
+        public double Balance { get; private set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public int AccountNumber { get; set; }
+        public int AccountNumber { get; private set; }
         public AccountType TypeOfBankAccount { get; set; }
         #endregion
 
@@ -29,5 +33,48 @@ namespace BedrockBank
         Savings,
         CD}
         #endregion
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        #region Constructors
+        public BankAccount()
+        {
+            lastAccountNumber = lastAccountNumber + 1;
+            AccountNumber = lastAccountNumber;
+
+           // (short hand way to write) AccountNumber = ++lastAccountNumber;
+
+        }
+
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        /// <returns>New balance</returns>
+
+        public double Deposit(double amount)
+        {
+            Balance = Balance + amount;
+            // (shortcut) Balance += amount; 
+            return Balance; 
+         
+        }
+        public double Withdraw(double amount)
+        {
+            if(amount>Balance)
+            {
+                throw new ArgumentOutOfRangeException("Not sufficient funds in the account");
+            }
+
+            Balance = Balance - amount;
+            return Balance;
+        }
+        #endregion
+
     }
 }
--- BedrockBank/Program.cs
+++ BedrockBank/Program.cs
@@ -10,6 +10,23 @@ namespace BedrockBank
     {
         static void Main(string[] args)
         {
+            BankAccount myAccount = new BankAccount();
+            //Set of the property is called
+            myAccount.Name = "Mary";
+            myAccount.Deposit(150.25);
+
+            //Get of the property is called
+            Console.WriteLine("Account number: {0}, Name: {1}, Balance: {2:c}",
+                myAccount.AccountNumber, myAccount.Name, myAccount.Balance);
+
+            BankAccount savingsAccount = new BankAccount();
+            savingsAccount.Name = "Mary 2";
+            savingsAccount.Deposit(200);
+
+            //Get of the property is called
+            Console.WriteLine("Account number: {0}, Name: {1}, Balance: {2:c}",
+                savingsAccount.AccountNumber, savingsAccount.Name, savingsAccount.Balance);
+
         }
     }
 }
